730bcab8b93d7112d2b369cbbb47b7e0c385a99a
[2기-1반] 김진경 <kimradium@naver.com>
2021-08-11 10:46:51 +0900

    #6 useDispatch()를 사용하여 action을 보냄

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from "react";
+import {Provider, useDispatch} from "react-redux";
+import store from "./redux/store";
+import { increment } from "./redux/action";
+
+// 1. dispatch에 action을 담아 reducer로 보낸다. => useDispatch()를 사용하여 action을 보내야 한다.
+
+function Count() {
+  const dispatch = useDispatch();
+
+
+  const handleClick = () => {
+    dispatch(increment());
+  }
+
+    return (
+      <div>
+        <button onClick={handleClick}>증가</button>
+        <p>카운트 : {}</p>
+      </div>
+    )
+}
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        
+      </div>
+    </Provider>
   );
 }
 
